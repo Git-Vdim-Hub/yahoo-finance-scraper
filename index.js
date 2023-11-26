24e9960378805743c0e8ab0efcf21a7a3b185f14
Git-Vdim-Hub <vadimrace@gmail.com>
2023-11-26 17:42:38 -0500

    added test code for functions

--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 const yahooFinance = require('yahoo-finance2').default; // NOTE the .default
 // gets financial results for apple stock
 getResults = async () => {
-    let results = await yahooFinance.quote('AAPL');
+    let results = await yahooFinance.quote('AAPL' );
     console.log(results);
 }
 //use "regularMarketPrice" for current price multiplied by Shares Owned to get the current value in the portfolio
 // use "dividendRate"/4 to get the quarterly dividend and /12 to get the monthly dividend per share.
 //use "dividendDate" for x-date
 
-getResults();
-//gets the dividend results for apple stock
+//getResults();
+// gets the dividend results for apple stock
 getDividendResults = async () => {
     const query = 'AAPL';
-    //period 1 provides the start date for the query, the second period provides the end date for the query, use shown format for data up to current date.
+//     //period 1 provides the start date for the query, the second period provides the end date for the query, use shown format for data up to current date.
     const queryOptions = { period1: '2019-05-08', /* ... */ };
-    const result = await yahooFinance._chart(query, queryOptions);
-    console.log(result.events);
+    const result = await yahooFinance.historical(query, queryOptions);
+    console.log(result);
 }
-//calls the function
+// //calls the function
 getDividendResults();
